f22c9690b13d8cf0ed133259cfb6e2799994cae6
ShianWang <984292420@qq.com>
2021-03-08 15:02:16 +0800

    feat: remove useless code

--- components/index.tsx
+++ components/index.tsx
@@ -2,9 +2,7 @@ export { default as Button } from './button';
 export { default as Cell } from './cell';
 export { default as Icon } from './icon';
 export { default as Input } from './input';
-
 export { default as Tabs } from './tabs';
-export { default as TabBar } from './tab-bar';
 export { default as LocaleProvider } from './locale-provider';
 export { default as ConfigProvider } from './config-provider';
 export { default as ConfigReceiver } from './config-receiver';
